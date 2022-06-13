f8daeed3aa02ebd2e391dc04f2cc995ec8b0e4c1
alex kimathi <keemalekie@gmail.com>
2022-06-13 19:00:56 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+   document.querySelector('form').addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    handleToDo(document.querySelector("#new-task-description").value)
+  })
 });
+
+function handleToDo(todo){
+  console.log(todo)
+  let ul= document.createElement('ul')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' X'
+  ul.textContent = `${todo} `
+  ul.appendChild(btn)
+  document.querySelector('#tasks').appendChild(ul)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
